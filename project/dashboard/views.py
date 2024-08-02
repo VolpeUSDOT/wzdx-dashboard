9f944b5e1e9ecf44dbd57e17a30f5917d6d68b50
Diego Temkin <diego.temkin@dot.gov>
2024-08-01 23:45:39 -0400

    refinements

--- project/dashboard/views.py
+++ project/dashboard/views.py
@@ -1,3 +1,5 @@
+from typing import Union
+
 from django.contrib.gis.db.models import Max, OuterRef, Subquery
 from django.core.paginator import Page, Paginator
 from django.http import Http404, HttpResponseRedirect
@@ -10,25 +12,47 @@ from .forms import SearchForm
 from .models import Feed, FeedStatus
 
 
-def get_page_button_array(paginator: Paginator, page: Page):
+def get_page_button_array(paginator: Paginator, page: Page) -> list[Union[int, str]]:
+    """Create what pages to use, based on guidelines from USWDS page https://designsystem.digital.gov/components/pagination/"""
     total_pages = paginator.num_pages
-    page_range = paginator.page_range
-    if total_pages < 5:
-        return page_range[0:total_pages]
+    current_page = page.number
+
+    if total_pages < 7:
+        return list(range(1, total_pages + 1))
+
     else:
-        current_page = page.number
-        if current_page < 4:
-            return page_range[0:5]
+        if current_page == 1 or current_page == 2 or current_page == 3:
+            return [1, 2, 3, 4, 5, "...", total_pages]
+        elif (
+            current_page == total_pages
+            or current_page == total_pages - 1
+            or current_page == total_pages - 2
+        ):
+            return [
+                1,
+                "...",
+                total_pages - 4,
+                total_pages - 3,
+                total_pages - 2,
+                total_pages - 1,
+                total_pages,
+            ]
         else:
-            first_page = current_page - 3
-            last_page = current_page + 5
-            return page_range[first_page:last_page]
+            return [
+                1,
+                "...",
+                current_page - 1,
+                current_page,
+                current_page + 1,
+                "...",
+                total_pages,
+            ]
 
 
 @method_decorator(csrf_exempt, name="dispatch")
 class FeedListView(ListView):
     model = Feed
-    paginate_by = 6
+    paginate_by = 5
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
