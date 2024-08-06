833ff6b0a39cccada574870c38d66376b3ca012f
Diego Temkin <diego.temkin@dot.gov>
2024-08-06 10:58:28 -0400

    switch to asgi, make docs page class-based, fix html issues, add edit button for staff

--- project/docs/urls.py
+++ project/docs/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 
-from . import views
+from .views import MarkdownContentView
 
 urlpatterns = [
-    path("", views.docs, name="docs"),
-    path("<slug:docspage>/", views.docs, name="docs"),
+    # path("", views.docs, name="docs"),
+    # path("<slug:docspage>/", views.docs, name="docs"),
+    path("", MarkdownContentView.as_view(), name="docs"),
+    path("<slug:docspage>/", MarkdownContentView.as_view(), name="docs"),
 ]
--- project/docs/views.py
+++ project/docs/views.py
@@ -1,31 +1,39 @@
-from django.db.models import BooleanField, ExpressionWrapper, F, Func, Q
-from django.http import Http404
-from django.shortcuts import get_object_or_404, render
+from typing import Any
+
+from django.db.models import BooleanField, ExpressionWrapper, Q
+from django.views.generic.detail import DetailView
 
 from .models import MarkdownContent
 
-
 # Create your views here.
-class Lower(Func):
-    function = "LOWER"
 
 
-def docs(request, docspage=""):
-    markdown_content = get_object_or_404(MarkdownContent, slug=docspage)
-    all_docs = (
-        MarkdownContent.objects.annotate(
-            current_page=ExpressionWrapper(
-                Q(slug__exact=docspage),
-                BooleanField(),
+class MarkdownContentView(DetailView):
+    model = MarkdownContent
+    template_name = "docs/markdown.html"
+    context_object_name = "markdown_content"
+
+    def get_object(self):
+        pk = self.kwargs.get(self.pk_url_kwarg)
+        slug = self.kwargs.get(self.slug_url_kwarg)
+
+        if pk is None and slug is None:
+            self.kwargs["slug"] = ""
+
+        return super().get_object()
+
+    def get_context_data(self, **kwargs: Any):
+        context = super().get_context_data(**kwargs)
+
+        context["all_docs"] = (
+            MarkdownContent.objects.annotate(
+                current_page=ExpressionWrapper(
+                    Q(slug__exact=self.kwargs["slug"]),
+                    BooleanField(),
+                )
             )
+            .values("title", "slug", "current_page")
+            .order_by("id")
         )
-        .values("title", "slug", "current_page")
-        .order_by("id")
-    )
 
-    context = {
-        "markdown_content": markdown_content,
-        "all_docs": all_docs,
-    }
-
-    return render(request, "docs/markdown.html", context=context)
+        return context
