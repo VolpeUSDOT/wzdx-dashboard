baa2db5fafc918e17a5ed31ee38f58872ba37971
Diego Temkin <diego.temkin@dot.gov>
2024-08-07 17:51:52 -0400

    expose api for mapping

--- project/api/api.py
+++ project/api/api.py
@@ -1,8 +1,10 @@
 from rest_framework import routers
 
-from .views import FeedPointsViewSet
+from .views import FeedDataViewSet, FeedPointsViewSet, FeedViewSet
 
 router = routers.DefaultRouter()
-router.register(r"points", FeedPointsViewSet)
+router.register(r"points", FeedPointsViewSet, basename="Points")
+router.register(r"data/(?P<feed>.+)", FeedDataViewSet, basename="FeedData")
+router.register(r"feeds", FeedViewSet, basename="Feeds")
 
 urlpatterns = router.urls
