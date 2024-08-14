0d5a83a2bfcb7b7c9f3e7380679ab39041897847
Diego Temkin <diego.temkin@dot.gov>
2024-08-14 18:51:41 -0400

    fix...

--- project/api/api.py
+++ project/api/api.py
@@ -3,7 +3,7 @@ from rest_framework import routers
 from .views import FeedPointsViewSet, FeedViewSet
 
 router = routers.DefaultRouter()
-router.register(r"points", FeedPointsViewSet, basename="points")
-router.register(r"feeds", FeedViewSet)
+router.register(r"points", FeedPointsViewSet, basename="api-points")
+router.register(r"feeds", FeedViewSet, basename="api-feeds")
 
 urlpatterns = router.urls
