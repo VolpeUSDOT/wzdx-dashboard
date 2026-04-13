ad1d086f833a24278e0223b1330e7a94fd6ad63d
Mark Mockett <mark.mockett@dot.gov>
2026-04-13 16:30:09 -0400

    Update settings.py to refresh static files using Manifest

--- project/src/settings.py
+++ project/src/settings.py
@@ -140,6 +140,13 @@ STATICFILES_DIRS = [
 
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
+STORAGES = {
+    "default": {"BACKEND": "django.core.files.storage.FileSystemStorage"},
+    "staticfiles": {
+        "BACKEND": "django.contrib.staticfiles.storage.ManifestStaticFilesStorage"
+    },
+}
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
